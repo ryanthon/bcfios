76fbc995e2dacabdb1b90df94e5db8e632dae02b
Wesley Yao <Wes@Wesleys-MacBook-Air.local>
2014-03-07 21:44:51 -0800

    Create event table

--- Bulletin/Classes/View Controllers/AddEventVC.m
+++ Bulletin/Classes/View Controllers/AddEventVC.m
@@ -7,8 +7,15 @@
 //
 
 #import "AddEventVC.h"
+#import "TextCell.h"
+
+static NSString *kTextCellID = @"textCell";
+static NSString *kTimeCellID = @"timeCell";
+static NSString *kSegueCell  = @"Cell";
 
 @interface AddEventVC ()
+
+@property (strong, nonatomic) NSArray *titleArray;
 
 @end
 
@@ -18,7 +25,37 @@
 {
     [super viewDidLoad];
     
-	
+    UINib *textCellNib = [UINib nibWithNibName:@"TextCell" bundle:nil];
+    [self.tableView registerNib:textCellNib forCellReuseIdentifier:kTextCellID];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *cellID = nil;
+    
+    switch( indexPath.row )
+    {
+        case 0: cellID = kTextCellID; break;
+        case 1: cellID = kTimeCellID; break;
+        case 2: cellID = kTimeCellID; break;
+        case 3: cellID = kSegueCell;  break;
+        case 4: cellID = kSegueCell;  break;
+    }
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    cell.textLabel.text = self.titleArray[indexPath.row];
+    
+    return cell;
 }
 
 - (IBAction)cancelButtonTap:(UIBarButtonItem *)sender
@@ -26,4 +63,12 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+- (NSArray *) titleArray
+{
+    if( !_titleArray )
+    {
+        _titleArray = @[@"Name", @"Start Time", @"End Time", @"Description", @"Location"];
+    }
+    return _titleArray;
+}
 @end
